b8b5a9aacda09a0118e48f54cddc60f0d19dcde4
Valeron01 <electronpochta3011@mail.ru>
2021-07-06 19:12:00 +0300

    added login page

--- juja/main/urls.py
+++ juja/main/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('get_json', views.ask_json),
-    path('login', views.login)
+    path('login', views.login),
 ]
--- juja/main/views.py
+++ juja/main/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.template.loader import render_to_string
 
 # Create your views here.
+
 
 def index(request):
     return render(request, 'index.html')
 
+
 def login(request):
-    pass
+    #rendered_page_html = render_to_string('login.html')
+
+    return render(request, 'login.html')#JsonResponse({'html': rendered_page_html})
+
 
 def ask_json(request):
     dict = {
-        'user':'Valeron',
-        'GET':request.GET,
-        'POST':request.POST
+        'user': 'Valeron',
+        'GET': request.GET,
+        'POST': request.POST
     }
-    
+
     return JsonResponse(dict)
