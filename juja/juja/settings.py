c577629000004fba0383ef4694fcb64f99e675b4
Valeron01 <electronpochta3011@mail.ru>
2021-07-08 20:03:11 +0300

    Added login by email

--- juja/juja/settings.py
+++ juja/juja/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+AUTHENTICATION_BACKENDS = ['juja.authentification.EmailBackend']
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
