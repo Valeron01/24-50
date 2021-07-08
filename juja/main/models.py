9e4d9ce9a4be1429f35fc09ee543d28aae711112
Valeron01 <electronpochta3011@mail.ru>
2021-07-08 19:54:47 +0300

    Added Some Tables: Product,

--- juja/main/models.py
+++ juja/main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    category = models.ForeignKey(Category, models.CASCADE)
+    price = models.FloatField()
+    seller = models.ForeignKey(User, models.CASCADE)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, models.CASCADE)
+    product = models.ForeignKey(Product, models.CASCADE)
+
+class UserDetail(models.Model):
+    user = models.ForeignKey(User, models.CASCADE)
+    balance = models.FloatField()
