8651c8b4e7350ca3dd24ee1b8dbef3706a60c455
Valeron01 <electronpochta3011@mail.ru>
2021-07-09 12:16:53 +0300

    merge fix

--- juja/main/urls.py
+++ juja/main/urls.py
@@ -10,12 +10,7 @@ urlpatterns = [
     path('exit', views.exit),
     path('auth', views.check_auth),
     path('offer', views.offer),
-<<<<<<< HEAD
     path('prodcuts', views.get_products),
     path('categories', views.get_categories),
     path('add_to_cart', views.add_to_cart),
-=======
-    path('products', views.get_products),
-    path('categories', views.get_categories)
->>>>>>> b6cba89bc18b0ebf2187cd9a5280eb3e20b99dec
 ]
